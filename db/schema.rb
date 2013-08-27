9afdbb8ab90cc26e0dc55626db119c279591fc8d
David Silver <dsilver@candidatemetrics.com>
2013-08-26 18:59:17 -0700

    add username

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130825145806) do
+ActiveRecord::Schema.define(:version => 20130827015807) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "username"
   end
+
+  add_index "users", ["username"], :name => "index_users_on_username"
 
 end
