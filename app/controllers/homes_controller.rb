17bee9213b6d6aba3c465e2cbb70be3a94fee923
David Silver <dsilver@candidatemetrics.com>
2013-08-27 12:56:41 -0700

    check logged in user

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,15 @@
 class HomesController < ApplicationController
+  before_filter :check_logged_in_user
+
   def show
 
+  end
+
+  private
+
+  def check_logged_in_user
+    if signed_in?
+      redirect_to dashboard_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shouter::Application.routes.draw do
   root to: 'homes#show', via: :get
+  resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 end
