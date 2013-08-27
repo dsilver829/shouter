49c8138eacd5e07b7047be6fb4788875b6354cff
David Silver <dsilver@candidatemetrics.com>
2013-08-26 19:00:08 -0700

    use strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
 
