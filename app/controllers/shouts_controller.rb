c7edc0c16282af401888cd5db9f076118f796cc8
David Silver <dsilver@candidatemetrics.com>
2013-08-31 22:25:02 -0400

    validate shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,8 +1,12 @@
 class ShoutsController < ApplicationController
   def create
     shout = current_user.shouts.build(shout_parameters)
-    shout.save
-    redirect_to dashboard_path
+    if shout.save
+      redirect_to dashboard_path
+    else
+      flash.alert = 'Could not shout.'
+      redirect_to dashboard_path
+    end
   end
 
   def show
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,3 +1,5 @@
 class Shout < ActiveRecord::Base
   belongs_to :user
+  default_scope { order "created_at DESC" }
+  validates :body, presence: true
 end
