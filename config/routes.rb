1aeb7da84ad74618f4805bdb93f53892dba79355
David Silver <dsilver@candidatemetrics.com>
2013-08-31 22:18:48 -0400

    show user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Shouter::Application.routes.draw do
   root to: 'homes#show', via: :get
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :shouts, only: [:create, :show]
 end
