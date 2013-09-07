9ee8085d8f6a07247e99126cda9e04243d0ebd27
David Silver <dsilver@candidatemetrics.com>
2013-09-07 11:04:46 -0400

    set up follower / following relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_many :shouts
+
+  has_many :followed_user_relationships, foreign_key: :follower_id, class_name: 'FollowingRelationship'
+  has_many :followed_users, through: :followed_user_relationships
+
+  has_many :follower_relationships, foreign_key: :followed_user_id, class_name: 'FollowingRelationship'
+  has_many :followers, through: :follower_relationships
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Shouter::Application.routes.draw do
   root to: 'homes#show', via: :get
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    post 'follow' => 'following_relationships#create'
+  end
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
