e7d0617fea71ce9089864cf54bc1bdbcab814c73
David Silver <dsilver@candidatemetrics.com>
2013-09-09 16:31:35 -0700

    refactor

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,8 @@
 class DashboardsController < ApplicationController
   def show
-    @text_shout = TextShout.new
-    @photo_shout = PhotoShout.new
-    @shouts = current_user.shouts
+    #@text_shout = TextShout.new
+    #@photo_shout = PhotoShout.new
+    #@shouts = current_user.shouts
+    @dashboard = Dashboard.new(current_user)
   end
 end
--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -1,12 +1,12 @@
 class FollowingRelationshipsController < ApplicationController
   def create
     current_user.follow user
-    redirect_to user
+    redirect_to user, notice: "Now following #{user.username}."
   end
 
   def destroy
     current_user.unfollow user
-    redirect_to user
+    redirect_to user, notice: "No longer following #{user.username}."
   end
 
   private
