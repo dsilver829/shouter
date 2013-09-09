bfe7650eaa913d88562e96736e6578741887282f
David Silver <dsilver@candidatemetrics.com>
2013-09-08 17:31:34 -0700

    refactor controller

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -1,13 +1,13 @@
 class FollowingRelationshipsController < ApplicationController
   def create
     user = User.find(params[:user_id])
-    current_user.followed_users << user
+    current_user.follow user
     redirect_to user
   end
 
   def destroy
     user = User.find(params[:user_id])
-    current_user.followed_users.delete(user)
+    current_user.unfollow user
     redirect_to user
   end
 end
