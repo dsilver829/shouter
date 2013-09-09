a6d5301c923144674bc2128933969d658b1776ce
David Silver <dsilver@candidatemetrics.com>
2013-09-08 17:33:08 -0700

    use bareword in controller

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -1,13 +1,17 @@
 class FollowingRelationshipsController < ApplicationController
   def create
-    user = User.find(params[:user_id])
     current_user.follow user
     redirect_to user
   end
 
   def destroy
-    user = User.find(params[:user_id])
     current_user.unfollow user
     redirect_to user
+  end
+
+  private
+
+  def user
+    User.find(params[:user_id])
   end
 end
