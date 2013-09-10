c367f30cd6c7ea6ee626c421b68522e962fd023a
David Silver <dsilver@candidatemetrics.com>
2013-09-10 08:54:39 -0700

    extract dashboard class

---
+++ app/models/dashboard.rb
@@ -0,0 +1,17 @@
+class Dashboard
+  def initialize user
+    @user = user
+  end
+
+  def new_text_shout
+    TextShout.new
+  end
+
+  def new_photo_shout
+    PhotoShout.new
+  end
+
+  def shouts
+    @user.shouts
+  end
+end
